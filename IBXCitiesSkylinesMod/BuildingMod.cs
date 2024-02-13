545c3fad0aa8a7a956a15227715b5bafc4fa8e0e
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2024-02-13 15:49:06 -0600

    Consumptions (not working)

---
+++ IBXCitiesSkylinesMod/BuildingMod.cs
@@ -0,0 +1,70 @@
+﻿using ICities;
+
+namespace NathanCitiesSkylinesMod
+{
+    public class BuildingMod : BuildingAI
+    {
+        private static bool ConsumeElectricity = true;
+        private static bool ConsumeWater = true;
+        private static bool AcumulateGarabage = true;
+
+        internal static void ToggleElectricityConsumption(bool value)
+        {
+            ConsumeElectricity = value;
+
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Warning, $"Consume Electricity {ConsumeElectricity}");
+        }
+
+        internal static void ToggleWaterConsumption(bool value)
+        {
+            ConsumeWater = value;
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Warning, $"Consume Water {ConsumeWater}");
+        }
+
+        internal static void ToggleGarabageAcumulation(bool value)
+        {
+            AcumulateGarabage = value;
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Warning, $"Acumulate Garabage {AcumulateGarabage}");
+        }
+
+        public override int GetElectricityConsumption()
+        {
+            if(ConsumeElectricity)
+            {
+                return base.GetElectricityConsumption();
+            }
+
+            return 0;
+        }
+
+        public override int GetWaterConsumption()
+        {
+            if(ConsumeWater)
+            {
+                return base.GetWaterConsumption();
+            }
+
+            return 0;
+        }
+
+        public override int GetGarbageRate(ushort buildingID, ref Building data)
+        {
+            if(AcumulateGarabage)
+            {
+                return base.GetGarbageRate(buildingID, ref data);
+            }
+
+            return 0;
+        }
+
+        public override int GetGarbageAmount(ushort buildingID, ref Building data)
+        {
+            if(AcumulateGarabage)
+            {
+                return base.GetGarbageAmount(buildingID, ref data);
+            }
+
+            return 0;
+        }
+    }
+}
--- IBXCitiesSkylinesMod/IBXCitiesSklyinesMod.cs
+++ IBXCitiesSkylinesMod/IBXCitiesSklyinesMod.cs
@@ -1,4 +1,5 @@
 ﻿using ICities;
+using UnityEngine;
 
 namespace NathanCitiesSkylinesMod
 {
@@ -6,5 +7,18 @@ namespace NathanCitiesSkylinesMod
     {
         public string Name { get { return "IBX Mod"; } }
         public string Description { get { return "This is an epic mod made by Nathan Schmitt (IBXCODECAT)"; } }
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            helper.AddGroup("Turn Simulation Features On or Off");
+            helper.AddCheckbox("Disable Electricity Consumption", false, (value) => BuildingMod.ToggleElectricityConsumption(!value));
+            helper.AddCheckbox("Disable Water Consumption", false, (value) => BuildingMod.ToggleWaterConsumption(!value));
+            helper.AddCheckbox("Disable Garabage Acumulation", false, (value) => BuildingMod.ToggleGarabageAcumulation(!value));
+
+            helper.AddSpace(250);
+
+            helper.AddGroup("Source Code");
+            helper.AddButton("View Code on GitHub", () => Application.OpenURL("https://github.com/IBXCODECAT/CitiesSkylines-SandboxMod"));
+        }
     }
 }
